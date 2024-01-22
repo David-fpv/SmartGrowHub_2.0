a742fe8694c5864534a0892a8a4b13fb03543f1b
David Tarasenko <davidtarasenko38@gmail.com>
2024-01-22 23:57:19 +0300

    Added new vector "sensors_data_"

--- include/setting.h
+++ include/setting.h
@@ -3,12 +3,14 @@
 #include <string>
 #include <vector>
 #include "component.h"
+#include "sensor.h"
 
 class Setting
 {
 private:
     std::string type_;
     std::vector<Component> components_;
+    std::vector<SensorData> sensors_data_;
 
 public:
     Setting(std::string type);
@@ -17,4 +19,6 @@ public:
     void SetType(std::string type);
 
     void addComponent(std::string type, double value, std::string unit);
+
+    void addSensorData(int sensor_id, int pin, std::string type, double value, std::string unit);
 };
