51651dd965d0b5b1f2a53236887b79f3549e51ee
David Tarasenko <davidtarasenko38@gmail.com>
2024-01-24 00:39:07 +0300

    Example main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,33 @@
 #include <Arduino.h>
+#include "data_collection.h"
+#include "setting.h"
+#include "information.h"
+
+//#define THRESHOLD 300
+#define THRESHOLD 263
+
+Setting smart_grow_hub();
+Information info;
+
+void readAllSensors();
 
 void setup() {
+    Serial.begin(9600);
+    initialization();
+    info.addSensorData(1, "Illumination", "%");
+    info.addSensorData(2, "Height", "cm");
 
 }
-
+   
 void loop() {
+    readAllSensors();
+    Serial.println(info.getStringJson());
+    delay(1000);
+}
 
+void readAllSensors()
+{
+    info.setValue(1, "Illumination", (double)readIllumination());
+    info.setValue(2, "Height", (double)readHeight());
 }
 
