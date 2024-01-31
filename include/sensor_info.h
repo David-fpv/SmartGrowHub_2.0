ed0d56e351851cfa17037a4d7cf91b3d44d7ef99
David Tarasenko <davidtarasenko38@gmail.com>
2024-01-31 23:32:01 +0300

    Add class SensorInfo for collect and store sensors info

---
+++ include/sensor_info.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+
+class SensorInfo
+{
+private: 
+    int sensor_id_;
+    std::string type_;
+    std::string unit_;
+    double (*function_) ();
+
+public:
+    SensorInfo(int sensor_id, std::string type, std::string unit, double (*function) ());
+
+    int GetSensorId();
+    std::string GetType();
+    std::string GetUnit();
+    double GetValue();
+};
---
+++ src/sensor_info.cpp
@@ -0,0 +1,33 @@
+#include "sensor_info.h"
+
+SensorInfo::SensorInfo(int sensor_id, std::string type, std::string unit, double (*function) ())
+{
+    sensor_id_ = sensor_id;
+    type_ = type;
+    function_ = function;
+    unit_ = unit;
+}
+
+
+int SensorInfo::GetSensorId()
+{
+    return sensor_id_;
+}
+
+
+std::string SensorInfo::GetType()
+{
+    return type_;
+}
+
+
+double SensorInfo::GetValue()
+{
+    return function_();
+}
+
+
+std::string SensorInfo::GetUnit()
+{
+    return unit_;
+}
