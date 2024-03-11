d471c30fb149edbaaf6b3223eb65ed6ee5848aea
David Tarasenko <davidtarasenko38@gmail.com>
2024-03-11 15:11:22 +0300

    Add new functions for sensors

--- include/sensor_functions.h
+++ include/sensor_functions.h
@@ -1,7 +1,17 @@
 #pragma once
+#include <Arduino.h>
 
 #include <TroykaMQ.h>
+
+#include <Wire.h>
+#include <Adafruit_AHTX0.h>
 
 void initialization();
 double readHeight();
 double readIllumination();
+double readSmoke();
+double readMethane();
+double readHydrogen();
+double readAirTemprature();
+double readAirHumidity();
+double readSoilHumidity();
--- src/sensor_functions.cpp
+++ src/sensor_functions.cpp
@@ -1,38 +1,41 @@
 #include "sensor_functions.h"
 
-#define PIN_MQ2  A0
+#define PIN_MQ2  34
 #define TRIG_PIN 13 // pin for Trig
 #define ECHO_PIN 14 // pin for Echo
 #define LDR_PIN 26 // пин для светорезистора
+#define SOIL_PIN 35 // датчик влажности почвы (water sensor)
 #define MIN_VALUE 0 // минимальное значение АЦП
 #define MAX_VALUE 4095 // максимальное значение АЦП
 
+Adafruit_AHTX0 aht;
+MQ2 mq2(PIN_MQ2);
 
 void initialization()
 {
-    // For MQ2
-    /*MQ2 mq2(PIN_MQ2);
-    mq2.calibrate(3.2);*/
-
+    mq2.calibrate(3.2);
+    aht.begin();
+    
     // For HC-04RC
     pinMode(TRIG_PIN, OUTPUT); 
     pinMode(LDR_PIN, INPUT);
 
 }
 
-/*void readMQ2()
-{
-     Serial.print("\tMethane: ");
-    Serial.print(mq2.readMethane());
-    Serial.print(" ppm");
-    Serial.print("\tSmoke: ");
-    Serial.print(mq2.readSmoke());
-    Serial.print(" ppm");
-    Serial.print("\tHydrogen: ");
-    Serial.print(mq2.readHydrogen());
-    Serial.println(" ppm");
-    delay(100);
-}*/
+double readSmoke()
+{    
+    return static_cast<double>(mq2.readSmoke());
+}
+
+double readHydrogen()
+{    
+    return static_cast<double>(mq2.readHydrogen());
+}
+
+double readMethane()
+{    
+    return static_cast<double>(mq2.readMethane());
+}
 
 double readHeight()
 {
@@ -50,6 +53,27 @@ double readIllumination()
     int value = analogRead(LDR_PIN);
     int percentage = map(value, MIN_VALUE, MAX_VALUE, 0, 100);
     return (double)percentage;
+}
+
+double readSoilHumidity()
+{
+    int value = analogRead(SOIL_PIN);
+    int percentage = map(value, MIN_VALUE, MAX_VALUE, 0, 100);
+    return (double)percentage;
+}
+
+double readAirTemprature()
+{
+    sensors_event_t humidity, temp;
+    aht.getEvent(&humidity, &temp);
+    return temp.temperature; 
+}
+
+double readHumidity()
+{
+    sensors_event_t humidity, temp;
+    aht.getEvent(&humidity, &temp);
+    return temp.relative_humidity; 
 }
 
 
