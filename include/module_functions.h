f9f2937b7ff66cdfe3ec84db832603416e18a488
David Tarasenko <davidtarasenko38@gmail.com>
2024-03-11 15:10:24 +0300

    Add functions for modules

---
+++ include/module_functions.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <Arduino.h>
+
+void initialization_module();
+void blink();
---
+++ src/module_functions.cpp
@@ -0,0 +1,14 @@
+#include "module_functions.h"
+
+#define PIN_LED 2
+
+void initialization_module()
+{
+    pinMode(PIN_LED, OUTPUT);
+}
+
+void blink()
+{
+
+}
+
